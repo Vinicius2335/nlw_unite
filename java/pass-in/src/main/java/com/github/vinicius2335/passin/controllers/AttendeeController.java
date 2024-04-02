90230873a40d3aad78c65e9f627e34d35e0577c6
Vinicius Vieira <vinicius2335@gmail.com>
2024-04-02 14:08:24 -0300

    feat(back): create controller to Attendee

---
+++ java/pass-in/src/main/java/com/github/vinicius2335/passin/controllers/AttendeeController.java
@@ -0,0 +1,11 @@
+package com.github.vinicius2335.passin.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendees")
+@RequiredArgsConstructor
+public class AttendeeController {
+}
