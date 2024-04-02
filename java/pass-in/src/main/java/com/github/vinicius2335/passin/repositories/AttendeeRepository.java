549d27e915fbe73222cbc4331315bc76841256f7
Vinicius Vieira <vinicius2335@gmail.com>
2024-04-02 16:38:50 -0300

    feat(back): add new query method to table attendees

--- java/pass-in/src/main/java/com/github/vinicius2335/passin/repositories/AttendeeRepository.java
+++ java/pass-in/src/main/java/com/github/vinicius2335/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,8 @@ package com.github.vinicius2335.passin.repositories;
 import com.github.vinicius2335.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    List<Attendee> findByEventId(String eventId);
 }
